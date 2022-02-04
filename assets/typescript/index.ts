2b49b52e7883fb0a3fbd8d3444eade58812a4158
Eduardo Lizarazu <jelo2021@gmail.com>
2022-02-04 15:22:24 -0400

    typescript

---
+++ assets/typescript/index.ts
@@ -0,0 +1,24 @@
+import MediaPlayer from "./MediaPlayer";
+import AutoPlay from "./plugins/AutoPlay"
+import AutoPause from "./plugins/AutoPause";
+
+const  video: HTMLElement = document.querySelector("video");
+const  toggleButtonPlay: HTMLElement = document.querySelector("#TogglePlay");
+const  toggleButtonMute: HTMLElement = document.querySelector("#ToggleMute");
+
+const player = new MediaPlayer({ 
+    el:  video, 
+    plugins : [new AutoPlay(), new AutoPause()] 
+});
+
+toggleButtonPlay.onclick = () =>  player.togglePlay();
+toggleButtonMute.onclick = () =>  player.toggleMute();
+
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js").catch(error => {
+        console.log(error.message)
+    })
+} else {
+    
+}
