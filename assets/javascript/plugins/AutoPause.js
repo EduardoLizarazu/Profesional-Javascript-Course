f7353752c7e97c41a57677da0ccbbbb9f2109a2f
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2022-02-04 11:26:21 -0400

    autopause

--- assets/javascript/plugins/AutoPause.js
+++ assets/javascript/plugins/AutoPause.js
@@ -1,16 +1,49 @@
-function AutoPause () {    }
+// function AutoPause () {  
+//     this.threshold = {  threshold: 0.25  };
+//     this.callback = this.callback.bind(this);
+// }
 
-AutoPause.prototype.pause = function () {
+// AutoPause.prototype.callback = function (entries, observer) {
+//     entries.forEach(entry => {
+//         if (entry.intersectionRatio >= this.threshold.threshold) {
+//             this.media.play();
+//         } else {
+//             this.media.pause();
+//         }
+//     });
+// }
+// AutoPause.prototype.run = function (player) {
+//     this.media = player.media;
+//     const observer = new IntersectionObserver(this.callback, this.threshold);
     
-    
+//     observer.observe(this.media);
+// }
+class AutoPause {
+    constructor() {
+        this.threshold = { threshold: 0.25 };
+        // solution to the pause(undefine) problem, the cause was the this context
+        // Now the context of this is the instance of the plugin
+        this.handleIntersection = this.handleIntersection.bind(this);
+    }
+
+    run(player) {
+        this.player = player;
+
+        const observer = new IntersectionObserver(this.handleIntersection, this.threshold);
+
+        observer.observe(this.player.media);
+    }
+
+    handleIntersection(entries) {
+        // only observe ONE property
+        const entry = entries[0];
+
+        const isVisibly = entry.intersectionRatio >= this.threshold.threshold;
+
+        isVisibly
+            ? this.player.play()
+            : this.player.pause();
+    }
 }
-AutoPause.prototype.run = function (player) {
-    // player.media
-    // const observer = new IntersectionObserver(player.pause, { threshold: 0.25 });
 
-
-
-    // observer.observe(player.media);
-    console.log("AutoPause Run");
-}
 export default AutoPause;
